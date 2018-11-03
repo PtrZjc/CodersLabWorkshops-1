e73bf81f8f5d5ec1b504133314d3d851a0e7793e
PtrZjc <ptr.zjc@gmail.com>
2018-11-03 21:06:13 +0100

    finished Most Popular Words

--- src/MostPopularWords/main.java
+++ src/MostPopularWords/main.java
@@ -6,28 +6,42 @@ import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
 
-import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
+import java.nio.file.*;
 import java.util.*;
 
 public class main {
+
+    static HashSet<String> fileNameSet = new HashSet<>();
+
     public static void main(String[] args) {
         parseMultipleFromWeb("input.txt");
-
+        findCommonWords();
     }
 
-//    static void findCommonWords(List<String>) {
-//
-//    }
+    static void findCommonWords() {
+        try {
+            Path filePath;
+            HashMap<String, Integer> countedWords = new HashMap<>();
+            for (String file : fileNameSet) {
+                filePath = Paths.get("src", "MostPopularWords", "WordsLibrary", file + ".txt");
+                for (String word : Files.readAllLines(filePath)) {
+                    countedWords.putIfAbsent(word, 0);
+                    countedWords.put(word, countedWords.get(word) + 1);
+                }
+            }
+            saveMapToFile(countedWords);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 
     static void parseMultipleFromWeb(String inputFile) {
         Path path = Paths.get("src", "MostPopularWords", inputFile);
         try {
             List<String> inputList = Files.readAllLines(path);
-            String[] inputData = new String[2];
+            String[] inputData;
             for (String inputLine : inputList) {
                 inputData = inputLine.split(" ");
                 parseFromWeb(inputData[0], inputData[1]);
@@ -40,12 +54,12 @@ public class main {
     static void parseFromWeb(String cssSelector, String url) {
         Connection connect = Jsoup.connect(url);
         Set<String> wordSet = new HashSet<>();
-        List<String> fileNameList = new LinkedList<>();
+
         try {
             Document document = connect.get();
             Elements titles = document.select(cssSelector);
             for (Element title : titles) {
-                for (String word : title.text().split("'[!()-:\\[\\]?„”\\\",.\\s]")) {
+                for (String word : title.text().split("[^0-9a-zA-ZąęćółśźżŚŹŻŁĆ']+")) {
                     if (word.length() > 3 && !Character.isDigit(word.charAt(0))) {
                         wordSet.add(word.toLowerCase());
                     }
@@ -59,23 +73,77 @@ public class main {
             words.add(word);
         }
         Collections.sort(words);
-//        words.stream().forEach(System.out::println);
         String filename = url.split("\\.")[1];
-        fileNameList.add(filename);
-        saveListToFile(words, filename);
-        saveListToFile(fileNameList, "fileNames");
+        fileNameSet.add(filename);
+        saveListToFile(words, filename, false);
     }
 
-    public static void saveListToFile(List words, String filename) {
-        Path path = Paths.get("src", "MostPopularWords", filename + ".txt");
+    public static void saveListToFile(List words, String filename, boolean append) {
+        Path path = Paths.get("src", "MostPopularWords", "WordsLibrary", filename + ".txt");
         try {
             if (!Files.exists(path)) {
                 Files.createFile(path);
             }
-            Files.write(path, words);
+            if (append) {
+                Files.write(path, words, StandardOpenOption.APPEND);
+            } else {
+                Files.write(path, words);
+            }
 
         } catch (Exception e) {
             e.printStackTrace();
+        }
+
+    }
+
+    public static void saveMapToFile(HashMap map) {
+        Path commonWords = Paths.get("src", "MostPopularWords", "CommonWords.txt");
+        List<String> tempWords = new LinkedList<>();
+        Iterator<Map.Entry<String, Integer>> iter;
+        BufferedWriter writer = null;
+        int counter = 0;
+
+        try {
+            if (!Files.exists(commonWords)) {
+                Files.createFile(commonWords);
+            }
+            Files.write(commonWords, "".getBytes());
+            writer = Files.newBufferedWriter(commonWords, StandardOpenOption.APPEND);
+
+            writer.write("The commonly occurring words were analyzed on the following websites:\n\n");
+            for (String file : fileNameSet) {
+                writer.write(file + "\n");
+            }
+            for (int i = fileNameSet.size(); i > 1; i--) {
+                writer.flush();
+                if (map.containsValue(i)) {
+                    writer.write("\n\nWords occurring in the quantity of " + i + ":\n\n");
+                } else {
+                    continue;
+                }
+
+                iter = map.entrySet().iterator();
+                while (iter.hasNext()) {
+                    Map.Entry<String, Integer> pair = iter.next();
+                    if (pair.getValue() == i) {
+                        tempWords.add(String.format("%-16s", pair.getKey()));
+                    }
+                }
+                Collections.sort(tempWords);
+                for (String word : tempWords) {
+                    writer.write(word);
+                    counter++;
+                    if (counter == 3) {
+                        writer.write("\n");
+                        counter = 0;
+                    }
+                }
+                tempWords.clear();
+                counter = 0;
+            }
+        } catch (
+                IOException e) {
+            e.getStackTrace();
         }
 
     }
